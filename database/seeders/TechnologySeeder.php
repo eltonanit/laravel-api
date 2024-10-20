cf5df90fd72f3688ef1818ca1eb3b24a8c73346e
eltonanit <elton.ani@gmail.com>
2024-10-20 21:06:06 +0200

    first commit

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $techs = ['HTML', 'CSS', 'Javascript', 'Bootstrap', 'Vuejs', 'Jquery', 'PHP', 'Laravel', 'Symfony', 'Reactjs', 'Nodejs', 'MySQL', 'XML'];
+        foreach($techs as $tech) {
+            $technology = new Technology();
+            $technology->name = $tech;
+            $technology->slug = Technology::generateSlug($tech);
+            
+            $technology->save();
+        }
+    }
+}
